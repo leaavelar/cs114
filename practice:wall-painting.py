136f15bb188dba61cc669cd41b8396f64f3e8833
s203 <s203@AI241LAB0203M11.local>
2017-10-25 11:51:27 -0700

    two new files

---
+++ practice:wall-painting.py
@@ -0,0 +1,7 @@
+print("Hello friend. I heard you are re painting one wall in your room. ")
+print("What is width of the wall?")
+width_total = input()
+print("What is the height of the wall?")
+height_total = input()
+print("How much does a gallon of paint costs at your local paint store?")
+paint_cost = input()
