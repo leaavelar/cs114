16e03f7962359003276180f78983b8ce267140a8
s203 <s203@AI241LAB0203M11.local>
2017-11-01 09:39:48 -0700

    new file

---
+++ magic8ball.py
@@ -0,0 +1,9 @@
+print("Magic 8 ball")
+import random
+print("Hello what is your name?")
+name = input()
+fortune_num: random.randint (1,9)
+if fortune_num == 1:
+    print("You will live a long life.")
+elif fortune_num == 2:
+    print("You ")
