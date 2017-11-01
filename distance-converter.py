66a9a0ed1943bfa8f6d918f36a1d06324e1f9fdc
s203 <s203@AI241LAB0203M11.local>
2017-11-01 11:58:51 -0700

    new file

---
+++ distance-converter.py
@@ -0,0 +1,9 @@
+print("Enter distance.")
+distance = int(input())
+print("Enter the units of the distance.")
+unit_distance = int(input())
+print("Enter target units.")
+target_unit = int(input())
+mile = distance * 1.609
+if mile >=100:
+    
