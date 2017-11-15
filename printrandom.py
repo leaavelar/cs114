a9076d4facb932e5d42951a9f480d300e086879e
s203 <s203@2024-01-203-11.local>
2017-11-15 09:20:46 -0800

    new file

---
+++ printrandom.py
@@ -0,0 +1,7 @@
+print("Hello")
+print("What is your name?")
+name = input()
+print(name, ",I will give you a random lucky number.")
+import random
+for i in range(1):
+    print(random.randint(1, 99))
